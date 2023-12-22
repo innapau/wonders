39b4409382463f3299c274ca704268833ed1f61d
Denis Maslakovich <maslakovich.denis@bztel.ru>
2023-12-22 11:08:04 +0300

    Rework game field

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import './App.css'
 
 const words = ['праздник', 'счастье', 'семья'];
+const rowElementsAmount = 18;
 
 function App() {
   const [word, setWord] = useState(words[0]);
   const [guess, setGuess] = useState('');
   const [gameFinished, setGameFinished] = useState(false);
   const [guessed, setGuessed] = useState([]);
-  const [guessClass, setGuessClass] = useState('');
 
   const nextWord = () => {
+    setGuessed(() => []);
     setWord((curr) => {
       const newIndex = words.indexOf(curr) + 1;
       if (newIndex < words.length - 1) {
@@ -27,11 +28,6 @@ function App() {
     setGuess(() => value);
     if (word.includes(value.toLowerCase()) && !guessed.includes(value)) {
       setGuessed((curr) => [...curr, value.toLowerCase()]);
-      setGuessClass(() => 'correct');
-    } else if (value === '') {
-      setGuessClass(() => '');
-    } else {
-      setGuessClass(() => 'wrong');
     }
   }
 
@@ -51,11 +47,29 @@ function App() {
     </div>)
   )
 
+  const getEmptyRow = (amount) => [...new Array(amount)].map((_, n) => (<div className={'letterBox empty'} key={n}></div>));
+
+  const defaultEmptyRow = getEmptyRow(rowElementsAmount);
+
+  const getLettersRow = () => {
+    const emptyLength = Math.ceil((18 -  word.split('').length) / 2);
+    const needToShorten = (emptyLength * 2) + word.split('').length > 18;
+    const emptyBoxes = [...new Array(emptyLength)].map((_, n) => (<div className={'letterBox empty'} key={n}></div>));
+    return [...getEmptyRow(needToShorten ? emptyLength - 1 : emptyLength), ...letters, ...emptyBoxes]
+  }
+
+  const lettersRow = getLettersRow();
+
   return (
     <>
       <div className={'game'}>
-        <h1 className={'gameName'}>Поле чудес</h1>
-        <div className={`lettersContainer ${guessClass}`}>{letters}</div>
+        <div className={'emptyLettersContainer'}>{defaultEmptyRow}</div>
+        <div className={'emptyLettersContainer'}>{defaultEmptyRow}</div>
+        <div className={'emptyLettersContainer'}>{defaultEmptyRow}</div>
+        <div className={'lettersContainer'}>{lettersRow}</div>
+        <div className={'emptyLettersContainer'}>{defaultEmptyRow}</div>
+        <div className={'emptyLettersContainer'}>{defaultEmptyRow}</div>
+        <div className={'emptyLettersContainer'}>{defaultEmptyRow}</div>
         <input
           className={'guessInput'}
           value={guess}
